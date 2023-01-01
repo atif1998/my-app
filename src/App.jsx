cdfb4d57caa257bba524d3296631846295cb39e3
atif1998 <115414985+atif1998@users.noreply.github.com>
2023-01-01 10:02:09 -0800

    assignment started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ComponentB } from "./components/ComponentB";
 
 function App() {
   return (
     <div className="App">
+      <ComponentB />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
